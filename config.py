632c7df11d2023932a89a030f65716db73e8d02a
Arnab Bhakta <ab.inpathtoadev@gmail.com>
2024-04-16 19:50:05 +0530

    simulation implementations

---
+++ config.py
@@ -0,0 +1,10 @@
+class Config:
+    N = 100
+    Q = 0.05  # malicious
+    P = 0.02  # trustworthy
+
+    INITIAL_RATING_MALICIOUS = 0.27
+
+    TOPICS = ["topic1", "topic2", "topic3", "topic4", "topic5"]
+    NUM_TRUE_NEWS = 5000
+    NUM_FALSE_NEWS = 5000
