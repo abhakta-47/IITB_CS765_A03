13119eb6718d91d3a55a58e1cc57084b8de71427
Arnab Bhakta <ab.inpathtoadev@gmail.com>
2024-04-17 20:46:40 +0530

    sudocode update

--- SudoCodes/Contracts.sol
+++ SudoCodes/Contracts.sol
@@ -1,38 +1,80 @@
 pragma solidity ^0.8.0;
 
-contract News {
+
+contract NewsDetection {
+
+    struct News{
+        address immutable author;
+        string immutable title;
+        string immutable body;
+        string[] immutable topics;
+        bytes32 immutable newsId;
+        uint immutable timestamp;
+    }
+
+    News immutable news;
+
+    uint constant deposit = 10 ether;
+
+    bool detectedAccuracy;
+    bool isLocked;
+    uint accuracy;
+    Vote[] votes;
+    uint private totalDeposit;
+
     struct Vote {
         address voter;
         bool voteValue;
     }
 
-    address public author;
-    string public title;
-    string public body;
-    string[] public topics;
-    bool public truthValue;
-    uint public timestamp;
-    bytes32 public newsId;
-    Vote[] public votes;
-
-    bool public detectedTruthy;
-    uint public accuracy;
-
-    constructor(address _author, string memory _title, string memory _body, string[] memory _topics, bool _truthValue) {
-        author = _author;
-        title = _title;
-        body = _body;
-        topics = _topics;
-        truthValue = _truthValue;
-        timestamp = block.timestamp;
-        newsId = keccak256(abi.encodePacked(_author, _title, _body, _topics, _truthValue, timestamp));
+    constructor(address _author, string memory _title, string memory _body, string[] memory _topics) {
+        news = News({
+            author: _author,
+            title: _title,
+            body: _body,
+            topics: _topics,
+            newsId: keccak256(abi.encodePacked(_author, _title, _body, block.timestamp)),
+            timestamp: block.timestamp
+        });
+        isLocked = false;
+    }
+    
+    function hasVoted(address _voter) private view returns (bool) {
+        for (uint i = 0; i < votes.length; i++) {
+            if (votes[i].voter == _voter) {
+                return true;
+            }
+        }
+        return false;
     }
 
     function castVote(address _voter, bool _voteValue) public {
+        require(!isLocked, "Voting is locked");
+        require(!hasVoted(_voter), "Already voted");
+        require(msg.value == deposit, "Incorrect deposit amount");
+        
+
         votes.push(Vote(_voter, _voteValue));
+        totalDeposit += deposit;
+
+        if (votes.length % 100 == 0) {
+            calculateAccuracy();
+        }
     }
 
-    function calculateAccuracy() public {
+    function calculateNewsAccuracyTopic(string memory _topic) private view returns (uint[2] memory) {
+        uint[2] memory newsAccuraciesTopic;
+        for (uint i = 0; i < votes.length; i++) {
+            if (votes[i].voteValue == true) {
+                newsAccuraciesTopic[0]++;
+            } else {
+                newsAccuraciesTopic[1]++;
+            }
+        }
+        return newsAccuraciesTopic;
+    }
+
+    function calculateAccuracy() private {
         uint[2] memory newsAccuracies;
         for (uint i = 0; i < topics.length; i++) {
             uint[2] memory newsAccuraciesTopic = calculateNewsAccuracyTopic(topics[i]);
@@ -44,68 +86,43 @@ contract News {
         uint totalFalseVotes = newsAccuracies[1];
         uint totalVotes = totalTrueVotes + totalFalseVotes;
 
-        if (totalVotes > 0) {
-            if (totalTrueVotes > totalFalseVotes) {
-                detectedTruthy = true;
-                accuracy = (totalTrueVotes * 100) / totalVotes;
-            } else {
-                detectedTruthy = false;
-                accuracy = (totalFalseVotes * 100) / totalVotes;
-            }
+        require(totalVotes > 0, "No votes cast");
+
+        if (totalTrueVotes/totalVotes > 0.6) {
+            detectedAccuracy = true;
+            accuracy = (totalTrueVotes * 100) / totalVotes;
+        } else if (totalFalseVotes/totalVotes > 0.6){
+            detectedAccuracy = false;
+            accuracy = (totalFalseVotes * 100) / totalVotes;
         } else {
-            detectedTruthy = truthValue;
-            accuracy = 0;
+            revert("Draw");
         }
+        distributeRewards();
+        lockVotes();
     }
 
-    function calculateNewsAccuracyTopic(string memory _topic) private view returns (uint[2] memory) {
-        uint[2] memory newsAccuraciesTopic;
+    function distributeRewards() private {
+        uint correctVotes = 0;
+        
         for (uint i = 0; i < votes.length; i++) {
-            if (votes[i].voteValue == truthValue) {
-                newsAccuraciesTopic[0]++;
-            } else {
-                newsAccuraciesTopic[1]++;
+            if (votes[i].voteValue == detectedAccuracy) 
+                correctVotes++;
+        }
+
+        uint rewardAmount = totalDeposit / correctVotes;
+
+        for (uint i = 0; i < votes.length; i++) {
+            if (votes[i].voteValue == detectedAccuracy) {
+                rewardVoter(votes[i].voter, rewardAmount);
             }
         }
-        return newsAccuraciesTopic;
-    }
-}
-
-
-contract Voter {
-    address public voterAddress;
-    string public voterType;
-    mapping(address => bool) public voted;
-
-    constructor() {
-        voterAddress = msg.sender;
     }
 
-    function vote(News news, bool voteValue) public {
-        require(!voted[msg.sender], "Already voted");
-        news.castVote(msg.sender, voteValue);
-        voted[msg.sender] = true;
+    function rewardVoter(address _voter, uint rewardAmount) private {
+        payable(_voter).transfer(rewardAmount);        
     }
-}
-
-contract Vote {
-    struct VoterInfo {
-        address voter;
-        uint amountDeposited;
-        address newsItem;
-        bool voteValue;
-    }
-
-    VoterInfo[] public votes;
-
-    event AccuracyCalculationTriggered(address indexed newsItem);
-
-
-    function vote(address _voter, uint _amountDeposited, address _newsItem, bool _voteValue) public {
-        votes.push(VoterInfo(_voter, _amountDeposited, _newsItem, _voteValue));
-
-        if (votes.length >= 100) {
-            emit AccuracyCalculationTriggered(_newsItem);
-        }
+    
+    function lockVotes() private {
+        isLocked = true;
     }
 }
