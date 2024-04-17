5dadf7a43cf2d7bcad3a0e14e297c366ceae33dd
Arnab Bhakta <ab.inpathtoadev@gmail.com>
2024-04-17 23:37:55 +0530

    updates

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 class Config:
-    N = 100
+    N = 100  # number of voters
     Q = 0.8  # malicious
     P = 0.05  # trustworthy
 
-    INITIAL_RATING_MALICIOUS = 0.0
+    INITIAL_RATING_MALICIOUS = 0.0  # initial rating for malicious voters
 
-    TOPICS = ["topic1", "topic2", "topic3", "topic4", "topic5"]
-    NUM_TRUE_NEWS = 5000
-    NUM_FALSE_NEWS = 5000
+    TOPICS = ["topic1", "topic2", "topic3", "topic4", "topic5"]  # topics list
+    NUM_TRUE_NEWS = 5000  # number of true news created
+    NUM_FALSE_NEWS = 5000  # number of false news created
